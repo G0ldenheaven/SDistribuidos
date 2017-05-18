bc839ec6bca67ebd9a6a27f78a1f33158c18f616
Goldenheaven <theshadow_@hotmail.com>
2017-05-18 22:21:35 +0100

    test

--- app.js
+++ app.js
@@ -35,8 +35,9 @@ app.enable('trust proxy');
 app.get('/', function(req, res) {    
     db.on('error', console.error.bind(console, 'connection error:'));
     
-    req.protocol=>'https';
+    //app.enable('trust proxy');
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.protocol);
     res.write(header);
     res.write(getMenu);
 
